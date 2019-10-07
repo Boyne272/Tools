1cc22cbc56e2e076e15944e1da5e8f4e00cc6e5f
Richard Boyne <31725818+Boyne272@users.noreply.github.com>
2019-10-07 10:57:09 +0200

    Fixed these methods to be standalone
    Could potentially improve the method to allow for multiple size lapalcaians or add assertions for better input validation

--- misc/mask_outline.py
+++ misc/mask_outline.py
@@ -3,33 +3,57 @@
 author: Richard Bonye (github Boyne272)
 Last updated on Mon Oct  7 10:32:24 2019
 """
-# needs fixing to be stand alone
-# imports need to be corrected
 
-def _outline(self, diag=True, original=False, multi=True):
-    """
-    Take the stored mask and use a laplacian convolution to find the
-    outlines for plotting. diag decides if diagonals are to be
-    included or not, original decides if the original mask should
-    be used or not.
-    multi is an option to do horizontal, vertical and multi_directional
-    laplacians and combine them. This is a safer method as particular
-    geometries can trick the above convolution.
-    """
-    # select the correcy arrays based on the options given
-    lap = self._laplacian_8 if diag else self._laplacian_4
-    mask = self.mask if not original else self.orig_mask
+import numpy as np
+from scipy.signal import convolve2d    
 
+# laplacian array
+LAP_8 = np.array([[1., 1., 1.],
+                  [1., -8., 1.],
+                  [1., 1., 1.]])
+LAP_4 = np.array([[0., 1., 0.],
+                  [1., -4., 1.],
+                  [0., 1., 0.]])
+LAP_H = np.array([[1., 2., 1.],
+                  [0., 0., 0.],
+                  [-1., -2., -1.]])
+LAP_V = np.array([[-1., 0., 1.],
+                  [-2., 0., 2.],
+                  [-1., 0., 1.]])
+
+LAP_HORIZONTAL  = np.ones((3, 3))
+
+def _outline(mask, multi=True, diag=True):
+    """
+    Take the given integer mask and use a laplacian convolution
+    to find the outlines. 
+    
+    Parameters
+    ----------
+    
+    mask : 2D array
+        The mask to find edges of
+        
+    multi : bool (optional)
+        If set to true a combination of horizontal and vertical laplacians
+        will be used to reduce the chance of particular mask geometries 
+        tircking the convolution algorithm.
+        
+    diag : bool (optional)
+        decides if diagonals are to be included in the laplacian
+    
+    """
+    
+    # create the laplacian
+    lap = LAP_8 if diag else LAP_4
+    
     # do the convolution to find the edges
     conv = convolve2d(mask, lap, mode='valid').astype(bool)
 
     if multi:
-        conv2 = convolve2d(mask, self._laplacian_h,
-                           mode='valid').astype(bool)
-        conv3 = convolve2d(mask, self._laplacian_v,
-                           mode='valid').astype(bool)
+        conv2 = convolve2d(mask, LAP_H, mode='valid').astype(bool)
+        conv3 = convolve2d(mask, LAP_V, mode='valid').astype(bool)
         conv = (conv + conv2 + conv3).astype(bool)
-
 
     # pad back boarders to have same shape as original image
     conv = np.pad(conv, 1, 'edge')
