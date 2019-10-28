3403eb8bb4ceb4c267d5b1e813df9d5e5462afd9
Richard Boyne <31725818+Boyne272@users.noreply.github.com>
2019-10-28 09:35:05 +0100

    The first cell from GitHub & Sphinx.ipynb
    as github often struggles to load notebooks

---
+++ notebooks/github_to_colab.py
@@ -0,0 +1,49 @@
+"""
+This is a small section fo code to pull a github repo into the google colab
+environment. Caution is needed as the password you give is stored in the git
+remotes, hence if you run anything like 'git remote -v' will show your password.
+Note if your github password has an @ in it then this will not work.
+"""
+
+
+import os
+from getpass import getpass
+
+
+# inputs
+username = "Boyne272"
+clone_url = "https://github.com/Boyne272/Tools.git"
+name = "Richard Boyne"
+email = "rmb115@ic.ac.uk"
+
+
+# remove sample data if it is there
+if os.path.isdir("sample_data"):
+    !rm -r sample_data
+
+
+# if we are not currently in a repo
+if not os.path.isdir(".git"):
+    
+    # get password
+    password = getpass('github password: ')
+    os.environ['GITHUB_AUTH'] = clone_url[:8] + username + ':' + password + '@' + clone_url[8:]
+    del password
+    
+    # clone the repo and move into it
+    !cp -r .config ..
+    !rm -r .config
+    !git clone $GITHUB_AUTH . #--quiet # reduce the output of clone
+    !cp -r ../.config .
+    !rm -r ../.config
+    
+    
+# set name and email for commits
+os.environ['GITHUB_AUTH'] = email
+!git config --global user.email $GITHUB_AUTH
+os.environ['GITHUB_AUTH'] = name
+!git config --global user.name $GITHUB_AUTH
+
+
+# show where we are
+!git show --summary
