e1c53345ed48508260300fbd72b685720b342ff3
Richard Boyne <31725818+Boyne272@users.noreply.github.com>
2020-02-03 11:58:05 +0000

    bug fix
    Also improved readability

--- python/dict_comp.py
+++ python/dict_comp.py
@@ -1,5 +1,6 @@
 """Quick function for comarping dicts element by element."""
 
+LINE = '\n\n' + '-'*20 + '\n\n'
 
 def dict_comp(a, b, pref=''):
     """Print non-equal key:value pairs (incl. nested dictionaries)."""
@@ -9,6 +10,8 @@ def dict_comp(a, b, pref=''):
     for k in a.keys():
         if isinstance(a[k], dict):
             dict_comp(a[k], b[k], pref=k+'.')
-        elif a[k] == b[k]:
-            print(pref + k, 'Not equal:')
-            print(a[k], b[k], '\n\n')
+            
+        print(pref + k, ' equal ', a[k] == b[k])
+        
+        if a[k] != b[k]:
+            print(LINE, a[k], LINE, b[k], LINE)
