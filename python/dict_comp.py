b1db9474d3540b33e88c4f63620f7532492a5af6
Richard Boyne <31725818+Boyne272@users.noreply.github.com>
2020-02-04 09:16:09 +0000

    improved formatting

--- python/dict_comp.py
+++ python/dict_comp.py
@@ -1,17 +1,27 @@
 """Quick function for comarping dicts element by element."""
 
-LINE = '\n\n' + '-'*20 + '\n\n'
+LINE = f'\n{"-"*38}\n'
 
-def dict_comp(a, b, pref=''):
+def dict_comp(a, b, a_name='dict_a', b_name='dict_b', pref=''):
     """Print non-equal key:value pairs (incl. nested dictionaries)."""
     if a.keys() != b.keys():
         raise KeyError('Dictionaries do not have matchine keys')
-
+        
+    if not pref:
+        print(f'{LINE[1:]}{"Key": <32}Equal{LINE}')
+        
+    diffs = []
+    
     for k in a.keys():
         if isinstance(a[k], dict):
             dict_comp(a[k], b[k], pref=k+'.')
         else:
-            print(pref + k, ' equal ', a[k] == b[k])
+            print(f'{pref + k: <32} {a[k] == b[k]}')
 
             if a[k] != b[k]:
-                print(LINE, a[k], LINE, b[k], LINE)
+                diffs.append((pref + k, a[k], b[k]))
+    
+    print(f'\n{LINE}\t\tDiffs{LINE}')
+    for element_name, el_a, el_b in diffs:
+        print(f'{a_name}.{element_name}:\t {el_a}{LINE}')
+        print(f'{b_name}.{element_name}:\t{el_b}{LINE}')
